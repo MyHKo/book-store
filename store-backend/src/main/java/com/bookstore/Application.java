31ac388138feb18b17dc7c3b24f4aa3b7d60111e
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-04 19:11:22 +0100

    Backend-init: add springboot dependencies to the backend

--- store-backend/src/main/java/com/bookstore/Application.java
+++ store-backend/src/main/java/com/bookstore/Application.java
@@ -1,2 +1,11 @@
-package com.bookstore;public class Application {
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
