27a507aec9696bed1354948955bd0461b3957c60
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-02-05 16:47:28 +0100

    Frontend: add simple request to the backend

--- store-frontend/src/App.jsx
+++ store-frontend/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () =>{
+    const [statement, setStatement] = useState("Loading...")
+
+    useEffect(() => {
+        fetch('http://localhost:8080/')
+            .then((response) => {
+                response.text().then(text => {setStatement(text)})
+            })
+            .catch((error) => {console.log(error.stackTrace)})
+    }, []);
+
     return (
-        <>Hello</>
+        <>{statement}</>
     )
 }
 
